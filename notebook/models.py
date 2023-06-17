8d7f5191fc2f2aa962ec3fd644cfa0e2fcb92c53
MonkeyareDragon <githubforme73@gmail.com>
2023-06-17 15:05:32 +0545

    New app notebook was created and models of both the app was finished.

---
+++ notebook/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Note(models.Model):
+    title = models.title = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.CharField(max_length=100, choices=(
+        ('Important', 'Important'),
+        ('Least Important', 'Least Important')
+    ))
+    posted_date = models.DateTimeField(default=timezone.now)
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title + ' - ' + self.poster.get_username()
