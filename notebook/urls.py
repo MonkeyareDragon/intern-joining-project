60e135e4439db9cf10444ead51fbd68151edcf4f
MonkeyareDragon <githubforme73@gmail.com>
2023-06-17 17:32:15 +0545

    Login and Register page was created for users

---
+++ notebook/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import (
+    PostListView,
+)
+from . import views
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='notebook-home'),
+]
--- notebook/views.py
+++ notebook/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from .models import Note
 
-# Create your views here.
+
+def home(request):
+    context = {
+        'notes': Note.objects.all()
+    }
+    return render(request, 'notebook/home.html', context)
+
+class PostListView(ListView):
+    model = Note
+    template_name = 'notebook/home.html'  
+    context_object_name = 'notes'
+    ordering = ['-posted_date']
+    paginate_by = 5
