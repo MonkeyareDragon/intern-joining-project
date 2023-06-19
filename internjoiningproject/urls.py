18a7c812a8940b57511e6e03d0bf8879979cbeb9
MonkeyareDragon <githubforme73@gmail.com>
2023-06-19 10:53:19 +0545

    Profile Picture can be added and seen in profile page

--- internjoiningproject/urls.py
+++ internjoiningproject/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import include, path
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('notebook.urls')),
@@ -27,3 +29,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', user_views.profile, name='profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
